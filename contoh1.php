e4155d77d86c459835c2c5f1a47e85f775d5b8b1
yudi <78812779+yudiyu@users.noreply.github.com>
2022-04-10 05:16:08 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "<p>Nama saya <strong>Yudi</strong> dan saya adalah seorang <strong>Mahasiswa Di Universitas Binasarana Informatika/UBSI</strong>.</p>";
+    }
+}
